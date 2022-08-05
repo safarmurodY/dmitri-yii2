78089edade7eeceab72c28a092a4d3bd9d891b84
cube <safarmurod20014@gmail.com>
2022-08-05 20:28:27 +0500

    some roll back

---
+++ frontend/controllers/auth/ResetController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace frontend\controllers\auth;
+
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\services\auth\PasswordResetService;
+use Yii;
+use yii\web\Controller;
+
+class ResetController extends Controller
+{
+    private $service;
+    public function __construct($id, $module, PasswordResetService $service, $config = [])
+    {
+        $this->service = $service;
+        parent::__construct($id, $module, $config);
+    }
+
+    public function actionRequest()
+    {
+        $form = new PasswordResetRequestForm();
+        if ($form->load($this->request->post()) && $form->validate()){
+            try {
+                $this->service->request($form);
+                Yii::$app->session->setFlash('success', Yii::t('app', 'check email'));
+                return $this->goHome();
+            } catch (\DomainException $e) {
+                \Yii::$app->errorHandler->logException($e);
+                \Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+        return $this->render('request', [
+            'model' => $form,
+        ]);
+
+    }
+
+    public function actionConfirm($token)
+    {
+        try {
+            $this->service->validateToken($token);
+        } catch (\DomainException $e) {
+            \Yii::$app->errorHandler->logException($e);
+            \Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+        $form = new ResetPasswordForm();
+        if ($form->load($this->request->post()) && $form->validate()){
+            try {
+                $this->service->reset($token, $form);
+                Yii::$app->session->setFlash('success', Yii::t('app', 'New Password'));
+            } catch (\DomainException $e) {
+                \Yii::$app->errorHandler->logException($e);
+                \Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+            return $this->goHome();
+        }
+        return $this->render('confirm', [
+            'model' => $form
+        ]);
+    }
+}
